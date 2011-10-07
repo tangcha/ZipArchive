b545eee215faae6587fe8499e93ac0daa6015df0
Sheng Luo <luosheng1986@gmail.com>
2011-10-07 15:28:26 +0800

    Create an Xcode project wrapper.

---
+++ ZipArchiveTests/ZipArchiveTests.h
@@ -0,0 +1,13 @@
+//
+//  ZipArchiveTests.h
+//  ZipArchiveTests
+//
+//  Created by Sheng Luo on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ZipArchiveTests : SenTestCase
+
+@end
---
+++ ZipArchiveTests/ZipArchiveTests.m
@@ -0,0 +1,32 @@
+//
+//  ZipArchiveTests.m
+//  ZipArchiveTests
+//
+//  Created by Sheng Luo on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZipArchiveTests.h"
+
+@implementation ZipArchiveTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ZipArchiveTests");
+}
+
+@end
