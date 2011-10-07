f267a46cfb0022675048e37b832b756d8d3f6c90
Sheng Luo <luosheng1986@gmail.com>
2011-10-07 15:50:55 +0800

    Create ZipUnarchive class.

---
+++ ZipArchive/ZipUnarchive.h
@@ -0,0 +1,13 @@
+//
+//  ZipUnarchive.h
+//  ZipArchive
+//
+//  Created by Sheng Luo on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZipUnarchive : NSObject
+
+@end
---
+++ ZipArchive/ZipUnarchive.m
@@ -0,0 +1,13 @@
+//
+//  ZipUnarchive.m
+//  ZipArchive
+//
+//  Created by Sheng Luo on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZipUnarchive.h"
+
+@implementation ZipUnarchive
+
+@end
